271016c816a01025891542158660534bec7489a4
jghoover <30665181+jghoover@users.noreply.github.com>
2018-05-15 12:06:07 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2 git emacs
+apt-get install -y apache2 git emacs python gcc gfortran sbcl build-essential libopenmpi-dev openmpi-bin
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
